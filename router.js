9a82fc1b0087d47dddd6e2864f1e9a451ea4d076
Daniel Bingham <daniel.bingham@ceros.com>
2018-05-19 12:09:01 -0400

    First pass of recipes REST endpoints.

--- router.js
+++ router.js
@@ -11,7 +11,7 @@ module.exports = function(database) {
      * The index route needs to return the html, css, and javascript for the SPA
      * front end.  It isn't considered a RESTful route in this case.
      ******************************************************************************/
-    router.get('/', function(request, response, next) {
+    router.get('/', function(request, response) {
       response.render('index', { title: 'Express' });
     });
 
@@ -20,32 +20,75 @@ module.exports = function(database) {
      ******************************************************************************/
 
     // Get a list of all recipes.
-    router.get('/recipes', function(request, response, next) {
-        database.query('select * from recipes', function(err, rows, fields) {
-            console.log(rows);
-        });
-
+    router.get('/recipes', function(request, response) {
+        database.query(
+            'select * from recipes', 
+            function(error, results, fields) {
+                if ( error ) {
+                    throw error;
+                }
+                response.json(results); 
+            }
+        );
     });
 
     // Create a new recipe
-    router.post('/recipes', function(request, response, next) {
+    router.post('/recipes', function(request, response) {
+        database.query(
+            'insert into recipes (title, source_url, created_date, update_date) values (?, ?, now(), now())', 
+            [request.params.title, request.params.source_url], 
+            function(error, results, fields) {
+                if ( error ) {
+                    throw error;
+                }
+                response.json({success: true, id: results.insertId});
+            }
+        );
 
     });
 
     // Get the details of a single recipe
-    router.get('/recipes/:recipe_id', function(request, response, next) {
-
+    router.get('/recipes/:id', function(request, response) {
+        database.query(
+            'select * from recipes where id=?', 
+            [request.params.id], 
+            function(error, results, fields) {
+                if ( error ) {
+                    throw error;
+                }
+                response.json(results);
+            }
+        );
     });
 
     // Edit an existing recipe.
-    router.post('/recipes/:recipe_id', function(request, response, next) {
+    router.post('/recipes/:id', function(request, response) {
+        database.query(
+            'update recipes set title = ? and source_url = ? where id = ?', 
+            [request.params.title, request.params.source_url, request.params.id],
+            function(error, results, fields) {
+                if ( error ) {
+                    throw error;
+                }
+                response.json({success: true});
+            }
+        );
 
     });
 
 
     // Delete an existing recipe.
-    router.delete('/recipes/:recipe_id', function(request, response, next) {
-
+    router.delete('/recipes/:id', function(request, response) {
+        database.query(
+            'delete from recipes where id = ?',
+            [ request.params.id ],
+            function(error, results, fields) {
+                if ( error ) {
+                    throw error;
+                }
+                response.json({success: true});
+            }
+        );
     });
 
     return router;
