b9cbb5a395a7f7034b1042407df037cab38b6961
Daniel Bingham <daniel.bingham@ceros.com>
2018-06-16 11:43:08 -0400

    Issue #1 - Skeleton of RecipeForm.

--- client/recipes/recipe-form.js
+++ client/recipes/recipe-form.js
@@ -7,37 +7,14 @@ class RecipeForm extends Component {
         this.state = {
             recipes: null
         };
-        this.loadRecipes = this.loadRecipes.bind(this);
 
     }
 
-    loadRecipes(recipes) {
-        this.setState({recipes: recipes});
-    }
 
     componentDidMount() {
-        fetch('/api/v0/recipes/')
-            .then(res => res.json())
-            .then(this.loadRecipes);
     }
 
     render() {
-        if ( this.state.recipes === null ) {
-            return (
-                <section className="recipes loading">
-                    Loading...
-                </section>
-            );
-        } else {
-            const recipes = this.state.recipes;
-            return (
-                <section className="recipes">
-                    {recipes.map(function(recipe, i) {
-                        return (<RecipeBox recipe={recipe}  key={recipe.id} />);
-                    })}
-                </section>
-            );
-        }
     }
 
 }
