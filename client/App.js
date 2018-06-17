f53f13f550bc441e539a9c380314d3e8856e77d4
Daniel Bingham <daniel.bingham@ceros.com>
2018-06-16 20:40:38 -0400

    Issue #1 - Removing RecipeForm from app.

--- client/App.js
+++ client/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
 import RecipeList from './recipes/recipe-list.js';
-import RecipeForm from './recipes/recipe-form.js';
 
 /**
  * App component acts as the root for the component tree, loading the layout and all other
